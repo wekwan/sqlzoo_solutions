dc18f24c78e39aa8b9037e3b5bb0301bcb49b52c
Wesley <wekwan@ucsd.edu>
2020-10-31 16:45:06 -0700

    reorganized files

--- help_desk.sql
+++ help_desk.sql
@@ -26,3 +26,26 @@ select date_format(shift_date, '%Y-%m-%d') as Shift_date,
 Shift_type, first_name, last_name
 from Shift join Staff on Shift.manager = Staff.staff_code
 order by Shift_date, Shift_type
+
+-- 6.
+select Company_name, count(*) as cc
+from Customer join Caller on Customer.company_ref = Caller.company_ref
+join Issue on Caller.caller_id = Issue.caller_id
+group by Company_name
+having cc > 18
+
+-- 7.
+select first_name, last_name
+from Caller
+where caller_id in
+(select Caller.caller_id
+ from Caller left join Issue on Caller.caller_id = Issue.caller_id
+ group by Caller.caller_id
+ having count(call_date) = 0)
+
+ -- 10.
+ select Staff.first_name, Staff.last_name, date_format(call_date, '%Y-%m-%d %T') as call_date
+ from Caller join Issue on Caller.caller_id = Issue.caller_id
+ join Staff on Staff.staff_code = Issue.taken_by
+ where Caller.first_name = 'Harry'
+ order by call_date desc limit 1
