d4636777b1263dd5eaef13457452e311fe389609
Wesley <wekwan@ucsd.edu>
2020-10-30 23:24:55 -0700

    finished part of help desk medium

--- help_desk_medium.sql
+++ help_desk_medium.sql
@@ -13,3 +13,10 @@ where caller_id in
  from Caller left join Issue on Caller.caller_id = Issue.caller_id
  group by Caller.caller_id
  having count(call_date) = 0)
+
+ -- 5.
+ select Staff.first_name, Staff.last_name, date_format(call_date, '%Y-%m-%d %T') as call_date
+ from Caller join Issue on Caller.caller_id = Issue.caller_id
+ join Staff on Staff.staff_code = Issue.taken_by
+ where Caller.first_name = 'Harry'
+ order by call_date desc limit 1
